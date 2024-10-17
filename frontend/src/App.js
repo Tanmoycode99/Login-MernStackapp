23dc592e17912f0dd1ea188187015ba5cc635c0f
Tanmoy Chakraborty <80977470+Tanmoycode99@users.noreply.github.com>
2024-10-18 02:35:00 +0530

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,30 +33,3 @@ const App = () => {
 
 export default App
 
-
-
-
-// import React from 'react';
-// import './App.css';
-// import { Route, Routes } from 'react-router-dom'; // Import BrowserRouter
-// import Login from './components/Login';
-// import Home from './components/Home'; // Assuming Home is a separate component
-// import UserInvalid from './components/UserInvalid';
-
-// const App = () => {
-//   return (
-   
-//     <>
-//     <div>
-//     <Login />
-
-//       <Routes>
-//         <Route path="/home" element={<Home />} />
-//       </Routes>
-//       </div>
-//       </>
-
-//   );
-// };
-
-// export default App;
