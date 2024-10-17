74a3b062212a312103c95d09aca95fb649c9f151
Tanmoy Chakraborty <80977470+Tanmoycode99@users.noreply.github.com>
2024-10-18 03:02:50 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,53 +1,49 @@
 const express = require('express');
-const mongoose= require('mongoose')
-const cors= require('cors')
-const userModel= require('./Models/user')
+const mongoose = require('mongoose');
+const cors = require('cors');
+const userModel = require('./Models/user');
+require('dotenv').config(); 
 
+const app = express();
 
-const app= express()
-require('dotenv').config()
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
+const dbURL = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/logindb";
 
-mongoose.connect("mongodb://127.0.0.1:27017/logindb");
+mongoose.connect(dbURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((err) => {
+  console.error("Error connecting to MongoDB:", err);
+});
 
-const PORT= process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-
-app.post('/login', (req, res) =>{
-
-    const {username, password} = req.body;
-    userModel.findOne({name: username})
-    .then(user =>{
-        if(user) {
-            if(user.password === password){
-                res.json("Success")
-            }
-            else{
-                res.json("The Password is Incorrect")
-            }
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    userModel.findOne({ name: username })
+      .then(user => {
+        if (user) {
+          if (user.password === password) {
+            res.json("Success");
+          } else {
+            res.json("The Password is Incorrect");
+          }
+        } else {
+          res.json("No record Found");
         }
-        else{
-            res.json("No record Found")
-        }
-    })
-})
+      });
+});
 
-
-
-
-
-app.post('/register' , (req, res) =>{
-
+app.post('/register', (req, res) => {
     userModel.create(req.body)
-    .then(users => res.json(users))
-    .catch(err => res.json(err))
-})
+      .then(users => res.json(users))
+      .catch(err => res.json(err));
+});
 
-
-app.listen(PORT, ()=> {
-
-    console.log(`Server is running on ${PORT}`)
-})
-
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
